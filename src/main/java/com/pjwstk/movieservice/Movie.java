b3e8905abaee9379e3502fb6441e1d4ae7e55bc0
adudek717 <a.dudek717@gmail.com>
2021-06-02 22:58:03 +0200

    trying to fix connection with database

--- src/main/java/com/pjwstk/movieservice/Movie.java
+++ src/main/java/com/pjwstk/movieservice/Movie.java
@@ -1,6 +1,14 @@
 package com.pjwstk.movieservice;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Movie {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private int id;
     private String name;
     private String category;
